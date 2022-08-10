bf96d6068d0d66da136e43f07c9b969678fc2eda
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-08-10 16:32:57 -0700

    clean up code and remove comments

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
-// on load
-// fetch all dogs
-// render and append all dog cards to the container
-
 async function loadData() {
     const dogs = await getDogs();
 
